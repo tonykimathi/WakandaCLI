cb4de5dff40062afe7b092e21b27afd7a97e5f57
Kazungu <kazungu61@gmail.com>
2018-03-02 11:30:01 +0300

    data for WakandaCLI

---
+++ model.py
@@ -0,0 +1,8 @@
+USER = 
+[{"id":"1", "username":"John23", "password":"1234","usertype":"normal"},
+{"id":"2", "username":"Sarah34", "password":"123rt4","usertype":"normal"},
+{"id":"3", "username":"Ben678", "password":"12yug34","usertype":"moderater"},
+{"id":"4", "username":"Girom45", "password":"12yug34","usertype":"moderater"},
+{"id":"5", "username":"Anna09", "password":"6789","usertype":"admin"},
+{"id":"3", "username":"Ben678", "password":"12yug34","usertype":"moderater"}]
+COMMENTS ={'John23':"I love this world",'Sarah34':"I love my next world",'Ben678':"I love this world",}
